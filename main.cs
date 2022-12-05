623a274c577ee8ed5410a210f9a252042c47c549
brunopaiva1 <brunopaivamaster10@gmail.com>
2022-12-05 00:19:53 -0300

    commit six

--- main.cs
+++ main.cs
@@ -6,6 +6,20 @@ namespace consoleTeste
     public class program{
         public static void Main(string[] args){
             var conta = new Conta();
+            conta.NomeConta = 'Conta Bancaria';
+            conta.NumConta = 12345678;
+            conta.Saldo = 1000;
+            conta.Agencia = 304;
+
+            conta.Titular = new Titular()  
+            {
+            Cpf = "12345678900",
+            Rg = 987654321,
+            Endereço = "Rua Central",
+            Nome = "Bruno Victor"
+            };
+
+            Console.WriteLine("Saldo atual: ", conta.VerSaldo());
         }
     }
 }
