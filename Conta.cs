0a2ddd0d04815a7ca3f8b16c365f03c3b16f83b5
brunopaiva1 <brunopaivamaster10@gmail.com>
2022-12-04 23:53:32 -0300

    first commit

---
+++ Conta.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collection.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Task;
+
+namespace consoleTeste.sistema
+{
+    public class Conta
+    {
+        public int Agencia {get ; set;}
+        public int NumConta {get; set;}
+        public string NomeConta {get; set;}
+        public Titular Titular {get; set;}
+        public Decimal Saldo {get; set;}
+        public Decimal VerSaldo(){
+            return Saldo;
+        }
+        
+    }
+    
+}
