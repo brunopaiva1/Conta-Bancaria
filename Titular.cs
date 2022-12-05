53bc6452dba4e1f762caf29351f66d665289a6cc
brunopaiva1 <brunopaivamaster10@gmail.com>
2022-12-05 00:30:32 -0300

    commit eight

---
+++ Titular.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collection.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Task;
+
+namespace consoleTeste.sistema
+{
+    public class Titular
+    {
+        public string Nome {get ; set;}
+        public string Cpf {get; set;}
+        public string Rg {get; set;}
+        public string Endereço {get; set;}
+    }
+    
+}
