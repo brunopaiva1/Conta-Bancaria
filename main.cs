357125dae7485c6e69d44f77101552b57d860d67
brunopaiva1 <brunopaivamaster10@gmail.com>
2022-12-05 00:10:58 -0300

    commit five

---
+++ main.cs
@@ -0,0 +1,12 @@
+using consoleTeste.sistema;
+using System;
+
+namespace consoleTeste
+{
+    public class program{
+        public static void Main(string[] args){
+            var conta = new Conta();
+        }
+    }
+}
+
